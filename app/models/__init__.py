28b6ce1a61708e3cb39dc43821c2260b74efa3b6
Ortiz05Ale <bautistaortizalejandra@gmail.com>
2025-06-04 19:52:21 -0600

    Add and delete imports in models and db

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,6 +1,3 @@
 from .user import User
-from .item import Item
-from .recycle_point import RecyclePoint
-from .message import Message
-from sqlalchemy.orm import relationship
+
 User.items = relationship("Item", back_populates="owner")
