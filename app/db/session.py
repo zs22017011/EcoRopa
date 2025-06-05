c1e50624ed60dd53cb7056827db18c85bff94900
Ortiz05Ale <bautistaortizalejandra@gmail.com>
2025-06-04 19:48:54 -0600

    Fixing imports

--- app/db/session.py
+++ app/db/session.py
@@ -1,6 +1,5 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
-from app.core.config import settings
+
 
 engine = create_engine(str(settings.DATABASE_URL), pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
